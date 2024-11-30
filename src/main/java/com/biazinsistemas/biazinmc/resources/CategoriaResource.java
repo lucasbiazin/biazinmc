af6c5930fc66298ef19e9d78a9bf647d52643b40
Lucas Biazin <lucasbiazin07@gmail.com>
2024-11-30 11:45:42 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/biazinsistemas/biazinmc/resources/CategoriaResource.java
+++ src/main/java/com/biazinsistemas/biazinmc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.biazinsistemas.biazinmc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.biazinsistemas.biazinmc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listAll () {
-		return "REST está ok!";
+	public List<Categoria> listAll () {
+	
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria (2, "Escritório");
 		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(cat1);
+		categorias.add(cat2);
+
+		
+		return categorias;
 	}
 }
