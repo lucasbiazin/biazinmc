2cbbbe277463e9fb3f541cddeb4d6e667eee0ff9
Lucas Biazin <lucasbiazin07@gmail.com>
2024-11-30 11:04:18 -0300

    Projeto criado

---
+++ src/main/java/com/biazinsistemas/biazinmc/BiazinmcApplication.java
@@ -0,0 +1,13 @@
+package com.biazinsistemas.biazinmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiazinmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiazinmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biazinsistemas/biazinmc/BiazinmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biazinsistemas.biazinmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiazinmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
