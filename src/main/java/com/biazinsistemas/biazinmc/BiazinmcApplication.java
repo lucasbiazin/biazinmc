71cc2934220d8db9cb27493921375c4c9140360d
Lucas Biazin <lucasbiazin07@gmail.com>
2024-12-01 18:16:50 -0300

    Produto e associacao many to many

--- src/main/java/com/biazinsistemas/biazinmc/BiazinmcApplication.java
+++ src/main/java/com/biazinsistemas/biazinmc/BiazinmcApplication.java
@@ -8,26 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.biazinsistemas.biazinmc.domain.Categoria;
+import com.biazinsistemas.biazinmc.domain.Produto;
 import com.biazinsistemas.biazinmc.repositories.CategoriaRepository;
+import com.biazinsistemas.biazinmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class BiazinmcApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository repository;
-	
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BiazinmcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 
-		repository.saveAll(Arrays.asList(cat1,cat2));
-		
-		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategoria().add(cat1);
+		p2.getCategoria().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategoria().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 }
