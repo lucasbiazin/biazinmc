610e6d45b869324909400b5dd2312c4f7de01b06
Lucas Biazin <lucasbiazin07@gmail.com>
2024-12-01 21:39:42 -0300

    Cliente, TipoCliente, Telefones e enderecos

--- src/main/java/com/biazinsistemas/biazinmc/BiazinmcApplication.java
+++ src/main/java/com/biazinsistemas/biazinmc/BiazinmcApplication.java
@@ -1,5 +1,6 @@
 package com.biazinsistemas.biazinmc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.biazinsistemas.biazinmc.domain.Categoria;
 import com.biazinsistemas.biazinmc.domain.Cidade;
+import com.biazinsistemas.biazinmc.domain.Cliente;
+import com.biazinsistemas.biazinmc.domain.Endereco;
 import com.biazinsistemas.biazinmc.domain.Estado;
 import com.biazinsistemas.biazinmc.domain.Produto;
+import com.biazinsistemas.biazinmc.domain.enums.TipoCliente;
 import com.biazinsistemas.biazinmc.repositories.CategoriaRepository;
 import com.biazinsistemas.biazinmc.repositories.CidadeRepository;
+import com.biazinsistemas.biazinmc.repositories.ClienteRepository;
+import com.biazinsistemas.biazinmc.repositories.EnderecoRepository;
 import com.biazinsistemas.biazinmc.repositories.EstadoRepository;
 import com.biazinsistemas.biazinmc.repositories.ProdutoRepository;
 
@@ -30,6 +36,12 @@ public class BiazinmcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	ClienteRepository clienteRepository;
+
+	@Autowired
+	EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BiazinmcApplication.class, args);
@@ -67,6 +79,30 @@ public class BiazinmcApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(es1, es2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cliente1 = new Cliente(null, "Lucas", "lucas@gmail.com", "11111111111", TipoCliente.PESSOAFISICA);
+		Cliente cliente2 = new Cliente(null, "Tiago", "tiago@gmail.com", "22222222222", TipoCliente.PESSOAFISICA);
+		Cliente cliente3 = new Cliente(null, "Carlos", "carlos@gmail.com", "33333333333", TipoCliente.PESSOAFISICA);
+		Cliente cliente4 = new Cliente(null, "Biazin Sistemas", "biazinsistemas@gmail.com", "55555555555555",
+				TipoCliente.PESSOAJURIDICA);
+
+		cliente1.getTelefones().addAll(Arrays.asList("44999991803", "44999999999"));
+		cliente2.getTelefones().addAll(Arrays.asList("44999991804", "44999999998"));
+		cliente3.getTelefones().addAll(Arrays.asList("44999991805", "44999999997"));
+		cliente4.getTelefones().addAll(Arrays.asList("44999991806", "44999999996"));
+
+		Endereco end1 = new Endereco(null, "Rua das araras", "10", "N/A", "CENTRO", "87240000", cliente1, c1);
+		Endereco end2 = new Endereco(null, "Rua das capivaras", "20", "N/A", "CENTRO", "87240000", cliente2, c2);
+		Endereco end3 = new Endereco(null, "Rua dos cachorro", "30", "N/A", "CENTRO", "87240000", cliente1, c2);
+		Endereco end4 = new Endereco(null, "Rua dos cavalo", "40", "N/A", "CENTRO", "87240000", cliente1, c3);
+
+		cliente1.getEnderecos().addAll(Arrays.asList(end1, end2));
+		cliente2.getEnderecos().addAll(Arrays.asList(end2, end3));
+		cliente3.getEnderecos().addAll(Arrays.asList(end3, end4));
+		cliente4.getEnderecos().addAll(Arrays.asList(end1));
+
+		clienteRepository.saveAll(Arrays.asList(cliente1, cliente2, cliente3, cliente4));
+		enderecoRepository.saveAll(Arrays.asList(end1, end2, end3, end4));
 
 	}
 }
